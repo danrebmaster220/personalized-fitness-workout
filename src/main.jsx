b07118ebb9c7d032897db4ebacb1736a2458d122
111111111111111 <111111111111111@gmail.com>
2025-11-06 10:00:09 +0800

    Login/Register Worked

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/personalized-fitness-workout">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
